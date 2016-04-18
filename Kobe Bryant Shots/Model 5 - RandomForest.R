c76b9631e3f573f71eb58f4524d7087a73deb8f3
jbtaylor1202 <jbtaylor12@hotmail.com>
2016-04-18 19:21:21 +0100

    Model 5 - Random Forest

---
+++ Kobe Bryant Shots/Model 5 - RandomForest.R
@@ -0,0 +1,35 @@
+﻿library(dplyr)
+library(ggplot2)
+library(randomForest)
+
+#Set working directory
+setwd("C:/Users/joet/Desktop/[CURRENT]/GitHub/Kaggle/Kobe Bryant Shots")
+
+set.seed(2012)
+
+#Read data file
+rawData<-read.csv(file = "data.csv")
+
+
+#Extract train and test data sets
+trainData<-rawData%>%
+  filter(!is.na(shot_made_flag))
+
+trainData$matchup<-substr(trainData$matchup,5,5)
+
+testData<-rawData%>%
+  filter(is.na(shot_made_flag))
+
+testData$matchup<-substr(testData$matchup,5,5)
+
+trainData$game_date<-unclass(trainData$game_date)
+trainData$action_type<-unclass(trainData$action_type)
+
+fit<-randomForest(shot_made_flag ~ combined_shot_type + shot_distance + shot_zone_basic, data=trainData, importance = TRUE, ntree = 2000)
+
+varImpPlot(fit)
+
+prediction <- predict(fit, testData)
+testData$shot_made_flag = prediction
+submit <- data.frame(shot_id = testData$shot_id, shot_made_flag=testData$shot_made_flag)
+write.csv(submit, file = "randomForest2.csv", row.names = FALSE)
